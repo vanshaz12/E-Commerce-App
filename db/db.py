d9436d20acaef30a7c30f9b52c10ecc021a08e79
Vanshaz12 <vshmstr826@gmail.com>
2023-05-11 21:41:57 +0930

    update

---
+++ db/db.py
@@ -0,0 +1,14 @@
+import os
+import psycopg2
+import psycopg2.extras
+
+DB_URL = os.environ.get('PGPASSWORD=GzfNU7ZyGnwN8BLxywNBe8qC77Jc4jNc psql -h dpg-ched1re7avja5m9ktlo0-a.oregon-postgres.render.com -U e_commerce_app_user e_commerce_app')
+
+def sql(query, parameters=[]):
+  connection = psycopg2.connect(DB_URL) # open connection
+  cursor = connection.cursor(cursor_factory=psycopg2.extras.RealDictCursor) # we use cursor to run SQL commands
+  cursor.execute(query, parameters) # begin transaction
+  results = cursor.fetchall()
+  connection.commit() # end transaction
+  connection.close() # close connection
+  return results
---
+++ db/schema.sql
@@ -0,0 +1,6 @@
+CREATE TABLE users(
+    id SERIAL PRIMARY KEY,
+    first_name TEXT,
+    last_name TEXT,
+    email TEXT
+    );
